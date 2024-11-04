d26cc7e9844f3f29e1966802afb6859950bf421b
Ryan Braz Tintore Viana <ryanviana@usp.br>
2024-11-04 20:58:30 -0300

    new code

---
+++ static/js/scripts.js
@@ -0,0 +1,18 @@
+// Function to copy URL to clipboard
+document.addEventListener("DOMContentLoaded", function () {
+  const copyButtons = document.querySelectorAll(".copy-button");
+
+  copyButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const url = this.getAttribute("data-url");
+      navigator.clipboard
+        .writeText(url)
+        .then(() => {
+          alert("URL copied to clipboard.");
+        })
+        .catch((err) => {
+          console.error("Could not copy text: ", err);
+        });
+    });
+  });
+});
